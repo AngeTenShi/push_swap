5748ea7295f23b3aab6b5979e374806ceb45612b
Ange <anggonza@student.42nice.fr>
2022-03-03 17:11:16 +0100

    utils

--- src/utils.c
+++ src/utils.c
@@ -6,13 +6,12 @@
 /*   By: anggonza <anggonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 14:19:41 by anggonza          #+#    #+#             */
-/*   Updated: 2022/03/03 15:52:02 by anggonza         ###   ########.fr       */
+/*   Updated: 2022/03/03 17:11:06 by anggonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-<<<<<<< HEAD:src/utils.c
 void	free_split(char **splitted)
 {
 	int	i;
@@ -24,14 +23,4 @@ void	free_split(char **splitted)
 		i++;
 	}
 	free(splitted);
-=======
-int	ft_parse_single(char *str)
-{
-	return (0);
-}
-
-int	ft_parse_multiple(char **str)
-{
-	return (0);
->>>>>>> 44aa5f4d10cce67947d7126d741cb8758843660e:src/parse.c
 }
